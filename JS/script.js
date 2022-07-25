d198e9868ac4d2bbd9dd33ab3e1feead2320da6e
dohaShiref <50174692+dohaShiref@users.noreply.github.com>
2022-07-25 12:46:18 +0200

    Update home page

--- JS/script.js
+++ JS/script.js
@@ -17,31 +17,34 @@ $('.owl-carousel').owlCarousel({
 
 
 function slideDown(el) {
-    
-    // var element=document.getElementById("cl1");
 
-    const accorHeaders=document.querySelectorAll(".headers");
-    accorHeaders.forEach(header=>{
-        header.addEventListener("click",event=>{
-            // header.classList.toggle("Rectangle-37-open");
-            var next = header.nextElementSibling;
-            if(header.classList.contains("Rectangle-37-open")){
-                header.classList.toggle("Rectangle-37-close");
-            }
-            else{
-                header.classList.toggle("Rectangle-37-open");
-            }
-            if (next.offsetHeight > 0) {
-                next.style.height = "0px";
-                
-              } else {
-                next.style.height = `${next.scrollHeight}px`;
-                header.classList.toggle("Rectangle-37-open");
-              }
-        })
-        
-    })
-  }
+    // if(window.innerWidth<767){
+        var next = el.nextElementSibling;
+            
+        if (next.offsetHeight > 0) {
+            next.style.height = "0px";
+            
+          } else {
+            next.style.height = `${next.scrollHeight}px`;
+          
+    }
+    
+    }
+
+    const megaMenumHeaders = document.querySelectorAll("MegaMenu-header");
+
+    megaMenumHeaders.forEach(Header => {
+        Header.addEventListener("mouseenter", event => {   
+                const accordionItemBody = Header.nextElementSibling;
+                if (accordionItemBody.offsetHeight==0) {
+                    accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + "px";
+                }
+                else {
+                    accordionItemBody.style.maxHeight = 0;
+                }
+
+            });
+        });
 
   const accordionItemHeaders = document.querySelectorAll(".accordionn-item-headerr");
 
